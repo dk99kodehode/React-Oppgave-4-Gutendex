d167e85b302523c4d90c8f7ad8a067c479de51b5
dk99kodehode <dk99kodehode@gmail.com>
2026-08-29 10:05:09 +0200

    routing changes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,19 +3,24 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Books from "../api/Gutendex";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Books />,
+        },
+        {
+          path: "search/:category/:query?",
+          element: <Books />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Books />,
-      },
-      {
-        path: "search/:category/:query?",
-        element: <Books />,
-      },
-    ],
+    basename: "/React-Oppgave-4-Gutendex",
   },
-]);
+);
